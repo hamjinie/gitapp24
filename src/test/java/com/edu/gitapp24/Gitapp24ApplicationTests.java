1e5bb075cfa6428e894c1f4d74d1670159c8316e
jini <gkawlsgml77@gmail.com>
2023-04-24 19:16:30 +0900

    프로젝트 생성 후 최초 커밋

---
+++ src/test/java/com/edu/gitapp24/Gitapp24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.gitapp24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitapp24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
